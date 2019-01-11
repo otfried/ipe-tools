a14cde316efedd192d06a7b4e7061bdd00768f2f
Otfried Cheong <otfried@ipe.airpost.net>
2019-01-11 14:45:25 +0100

    Added control files for building Linux packages

---
+++ obs/make-specs.sh
@@ -0,0 +1,38 @@
+#
+# Produce ipe.dsc and ipe_x.y.z-d_debian.tar.xz
+#
+
+IPEVERS="7.2.8"
+DEBVERS="1"
+
+SOURCES=~/ipe-$IPEVERS-src.tar.gz
+
+ORIGFILE=ipe_$IPEVERS.orig.tar.gz
+DEBFILE=ipe_$IPEVERS-${DEBVERS}_debian.tar.xz
+DSCFILE=ipe.dsc
+
+tar cJvf $DEBFILE debian
+cat <<EOF > $DSCFILE
+Format: 3.0 (quilt)
+Source: ipe
+EOF
+
+echo "Version: $IPEVERS-$DEBVERS" >> $DSCFILE
+
+cat <<EOF >> $DSCFILE
+Binary: ipe
+Maintainer: Otfried Cheong <otfried@ipe.otfried.org>
+Architecture: any
+Homepage: http://ipe.otfried.org/
+Build-Depends: sharutils, debhelper (>= 9~), zlib1g-dev, qtbase5-dev, qtbase5-dev-tools, libfreetype6-dev, libcairo2-dev, libjpeg-dev, libpng-dev, liblua5.3-dev
+Files:
+EOF
+
+cp $SOURCES $ORIGFILE
+orig1=`md5sum $ORIGFILE | cut -d" " -f1`
+orig2=`stat --printf="%s" $ORIGFILE`
+deb1=`md5sum $DEBFILE | cut -d" " -f1`
+deb2=`stat --printf="%s" $DEBFILE`
+
+echo "" $orig1 $orig2 $ORIGFILE >> $DSCFILE
+echo "" $deb1 $deb2 $DEBFILE >> $DSCFILE
