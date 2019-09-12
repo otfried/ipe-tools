a578aa7ae87a69858ec42f31a6608e98510ba417
Otfried Cheong <otfried@ipe.airpost.net>
2019-09-12 23:02:57 +0200

    Poweripe is now working and can convert simple Latex markup

--- poweripe/poweripe.py
+++ poweripe/poweripe.py
@@ -4,15 +4,18 @@
 # Translate an Ipe presentation to Powerpoint
 #
 
-import sys, os
-import datetime, argparse
+import sys, os, re, datetime, argparse
 
 import pptx
 from pptx.util import Pt
 from pptx.enum.shapes import MSO_SHAPE
 from pptx.enum.text import PP_ALIGN, MSO_AUTO_SIZE, MSO_ANCHOR
 
-from pylatexenc.latexwalker import LatexWalker, LatexEnvironmentNode
+# TODO: maybe use a better Latex parser
+# from pylatexenc.latexwalker import LatexWalker, LatexEnvironmentNode
+
+from PIL import Image
+from lxml import etree
 
 import ipe as Ipe
 ipe = Ipe.ipe()
@@ -42,11 +45,19 @@ latex_sizes = {
   "\Huge", 25,
   }
 
+def addBulletToParagraph(p):
+  size = p.font.size or Pt(18)
+  el = p._pPr
+  el.set("marL", "%d" % size)
+  el.set("indent", "-%d" % size)
+  nsmap = el.nsmap
+  el.insert(0, etree.Element("{" + nsmap['a'] + "}buChar", char="\u2022"))
+
+# --------------------------------------------------------------------
+
 class PowerIpe:
   def __init__(self, args):
     self.args = args
-    if self.args.latex:
-      self.args.text = True
 
   def findColors(self):
     s = self.sheets.allNames(self.sheets, "color")
@@ -82,17 +93,33 @@ class PowerIpe:
     os.system("iperender -svg -nocrop %s %s" % (tmpname, svgname))
     os.system("iperender -png -nocrop -transparent %s %s" % (tmpname, pngname))
     pic = slide.shapes.add_svg_picture(svgname, pngname, 0, 0)
-  
+
   def convertText(self, slide, obj):
-    s = obj.text(obj)
+    s = obj.text(obj).strip()
     textStyle = obj.get(obj, "textstyle")
     textSize = obj.get(obj, "textsize")
     color = obj.get(obj, "stroke")
+    italic = False
+    bold = False
+    if s.startswith(r"\bfseries"):
+      s = s[9:].lstrip()
+      bold = True
+    if s.startswith(r"\itshape"):
+      s = s[8:].lstrip()
+      italic = True
+    parSplitRe = "\n[ \t]*\n|\\\\par\\W*"
+    if textStyle == "itemize" and s.startswith(r"\item"):
+      s = s[5:].lstrip()
+      parSplitRe += "|\\\\item"
+      textStyle = "item"
+    pars = re.split(parSplitRe, s)
+    pars = [ s.replace("\n", " ") for s in pars ]
+    if len(pars) == 0:
+      return # huh?
     box = ipe.Rect()
     obj.addToBBox(obj, box, ipe.Matrix())
     pos = box.topLeft(box)
-    # why do we need this correction factor?
-    size = sizeMap.get(textSize, sizeMap["normal"]) * 0.85
+    size = sizeMap.get(textSize, sizeMap["normal"])
     #helper = slide.shapes.add_shape(MSO_SHAPE.RECTANGLE, Pt(self.x0 + pos.x), Pt(self.y0 - pos.y),
     # Pt(box.width(box)), Pt(box.height(box)))
     txBox = slide.shapes.add_textbox(Pt(self.x0 + pos.x), Pt(self.y0 - pos.y - 0.2 * size),
@@ -105,30 +132,83 @@ class PowerIpe:
     tf.margin_right = 0
     tf.margin_bottom = 0
     tf.vertical_anchor = MSO_ANCHOR.TOP
-    p = tf.paragraphs[0]
-    p.text = s.strip()
-    p.font.size = Pt(size)
-    p.line_spacing = Pt(1.2 * size)
-    if textStyle == "center":
-      p.alignment = PP_ALIGN.CENTER
-    elif textStyle == "item":
-      p.level = 1
-    if color in self.colors:
-      p.font.color.rgb = self.colors[color]
+    for i in range(len(pars)):
+      s = pars[i].strip()
+      s = re.sub(r"\\\\[ \t\n\r\f\v]*", "\v", s)
+      if i == 0:
+        p = tf.paragraphs[0]
+      else:
+        p = tf.add_paragraph()
+      p.text = s
+      p.font.size = Pt(size)
+      p.line_spacing = Pt(1.2 * size)
+      if textStyle == "center":
+        p.alignment = PP_ALIGN.CENTER
+      elif textStyle == "item":
+        addBulletToParagraph(p)
+      if color in self.colors:
+        p.font.color.rgb = self.colors[color]
+      p.font.bold = bold
+      p.font.italic = italic
+      if "\\textcolor{" in s:
+        pos = 0
+        r = p
+        pat = re.compile("\v|\\\\textcolor{([a-zA-Z0-9]+)}{([^}]+)}")
+        while True:
+          m = pat.search(s, pos)
+          if m is None:
+            r.text = s[pos:]
+            break
+          r.text = s[pos:m.start()]
+          if m.lastindex is None:
+            p.add_line_break()
+          else:
+            r = p.add_run()
+            r.text = m.group(2)
+            runColor = m.group(1)
+            if runColor in self.colors:
+              r.font.color.rgb = self.colors[runColor]
+          r = p.add_run()
+          pos = m.end()
 
   # determine if we want to convert this text object
   def wantConvertText(self, obj):
-    if not self.args.text:
+    if self.args.no_text:
       return False
     mp = obj.get(obj, "minipage")
     if not mp:
       return False
     s = obj.text(obj)
-    if not self.args.latex:
-      if "\\" in s or "$" in s:
-        return False
-    return True
+    if self.args.latex:
+      return True
+    # allow some simple latex
+    s = s.strip()
+    if s.startswith("\\itshape"):
+      s = s[8:]
+    if s.startswith("\\bfseries"):
+      s = s[9:]
+    s = s.replace(r"\\", "")
+    s = s.replace(r"\textcolor", "")
+    s = s.replace(r"\item", "")
+    s = s.replace(r"\par", "")
+    return not ("\\" in s or "$" in s)
 
+  def embedImage(self, slide, obj):
+    info = obj.info(obj)
+    s = str(obj) # make a unique name
+    imgname = "tmpipe" + s[s.index("@"):]
+    ext = ".jpg" if info.format == "jpg" else ".ppm"
+    obj.savePixels(obj, imgname + ext)
+    if ext == ".ppm":
+      im = Image.open(imgname + ext)
+      ext = ".png"
+      im.save(imgname + ext)
+    box = ipe.Rect()
+    obj.addToBBox(obj, box, ipe.Matrix())
+    pos = box.topLeft(box)
+    pic = slide.shapes.add_picture(imgname + ext, Pt(self.x0 + pos.x), Pt(self.y0 - pos.y),
+                                   Pt(box.width(box)), Pt(box.height(box)))
+    
   def convertPage(self, pageNo, viewNo):
     sys.stderr.write("Converting page %d\n" % pageNo)
     p = self.doc[pageNo]
@@ -148,8 +228,8 @@ class PowerIpe:
       ty = obj.type(obj)
       if ty == "text" and self.wantConvertText(obj):
         pass
-      #elif ty == "image":
-      #  info = obj.info(obj)
+      elif ty == "image":
+        self.embedImage(slide, obj)
       else:
         svgobjs.add(i)
     self.addSvg(slide, pageNo, svgobjs)
@@ -184,7 +264,6 @@ class PowerIpe:
     layout = self.sheets.find(self.sheets, "layout")
     self.x0 = layout.origin.x
     self.y0 = layout.origin.y + layout.papersize.y
-    print(layout.origin, layout.papersize, self.x0, self.y0)
   
     self.prs.slide_width = Pt(layout.papersize.x)
     self.prs.slide_height = Pt(layout.papersize.y)
@@ -205,7 +284,7 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="Convert an Ipe presentation into pptx format")
   parser.add_argument('ipefile', help="File name for Ipe input")
   parser.add_argument('--output', help="File name for pptx output")
-  parser.add_argument('--text', help="Convert text containing no Latex", action='store_true')
+  parser.add_argument('--no-text', help="Do not convert text objects", action='store_true')
   parser.add_argument('--latex', help="Try to convert Latex text", action='store_true')
 
   powerIpe = PowerIpe(parser.parse_args())
--- poweripe/readme.md
+++ poweripe/readme.md
@@ -6,16 +6,22 @@ bosses/colleagues/clients keep asking for Powerpoint files?
 Fear no more!  Poweripe is a Python script that translates an Ipe
 presentation into a Powerpoint presentation.
 
+Poweripe requires Ipe 7.2.13 or later.
 
 ## Installation
 
 Poweripe requires some Python modules:
 
-1. Install `pylatexenc` by saying `pip3 install pylatexenc`. (This module is used to parse Latex.)
+<!--
+1. Install `pylatexenc` by saying `pip3 install pylatexenc`. (This
+module is used to parse Latex.)
+-->
 
-2. Install the [ipepython](https://github.com/otfried/ipe-tools/tree/master/ipepython) module. (This module is used to read Ipe files.)
+1. Install the
+[ipepython](https://github.com/otfried/ipe-tools/tree/master/ipepython)
+module. (This module is used to read Ipe files.) 
 
-3. Install the `python-pptx` module (a module for creating Powerpoint documents). 
+2. Install the `python-pptx` module (a module for creating Powerpoint documents). 
 Currently, you need to get my fork of this module, with added SVG
 support.  You can find it at
 https://github.com/otfried/python-pptx. Make sure to get the **svg-pictures** branch!
@@ -28,27 +34,31 @@ pip3 install python-pptx
 (See https://python-pptx.readthedocs.io/ for details.)
 -->
 
-## Running
+## Usage
 
 You run Poweripe from the command line like this:
 ```
-python3 poweripe.py presentation.pdf
+python3 poweripe.py --no-text presentation.pdf
 ```
-The output will be stored in `presentation.pptx`.  Alternatively, provide the `--output` option to select a different output file.
+The output will be stored in `presentation.pptx`.  Alternatively,
+provide the `--output` option to select a different output file. 
 
-In this simplest mode, Poweripe stores all the contents of the Ipe
-document as an image in the pptx output.  The image contains both an
-SVG-version, which gives high-quality vector graphics, and a
-lower-resolution bitmap as a fallback.  Recent Powerpoint versions
-will display the SVG contents, but it seems that Libreoffice does not
-yet support SVG and shows the bitmap version instead.
+In this simplest mode, Poweripe stores **all** the contents of the Ipe
+document, including all text, as *graphics* in the pptx output.  There
+is both an SVG-version of this graphics, which gives high-quality
+vector output, and a lower-resolution bitmap as a fallback.  Recent
+Powerpoint versions will display the SVG contents, but it seems that
+Libreoffice does not yet support SVG and shows the bitmap version
+instead.
 
-With the `--text` option, Poweripe will convert all minipage text
-objects that do not contain Latex markup into text objects that can be
-edited in the pptx file.
+If you remove `--no-text` option, Poweripe will convert all minipage
+text objects contain only simple Latex markup into text objects that
+can be edited in the pptx file.
 
 With the `--latex` option, Poweripe will also convert minipage text
-objects that contain Latex markup.  Translating some simple Latex
-commands (for color, emphasis, and itemizing) is planned for the near
-future.
+objects that contain Latex markup.  You will have to edit the
+resulting pptx file to make it useful.
 
+Since the `python-pptx` library does not yet support making slides
+that build up incrementally, Poweripe currently converts only the
+**last view** of each page.
