0e6eeca189ea6d96fe1168b4711af956057636f8
Otfried Cheong <otfried@ipe.airpost.net>
2019-09-12 14:48:42 +0200

    Conversion without Latex markup is working now

--- poweripe/poweripe.py
+++ poweripe/poweripe.py
@@ -5,11 +5,12 @@
 #
 
 import sys, os
-import datetime
+import datetime, argparse
 
 import pptx
 from pptx.util import Pt
-from pptx.enum.text import PP_ALIGN
+from pptx.enum.shapes import MSO_SHAPE
+from pptx.enum.text import PP_ALIGN, MSO_AUTO_SIZE, MSO_ANCHOR
 
 from pylatexenc.latexwalker import LatexWalker, LatexEnvironmentNode
 
@@ -20,28 +21,43 @@ assert sys.hexversion >= 0x3060000
 
 # --------------------------------------------------------------------
 
+# these values are from 'official' Ipe presentation stylesheet
+# TODO: compute based on actual stylesheet
 sizeMap = { "huge": 2.8 * 17,
             "large": 2.8 * 12,
-            "normal": 28,
+            "normal": 2.8 * 10,
+            "small" : 2.4 * 10,
 }
 
-# from size10.clo:
-"""
-\newcommand\tiny{\@setfontsize\tiny\@vpt\@vipt}
-\newcommand\large{\@setfontsize\large\@xiipt{14}}
-\newcommand\Large{\@setfontsize\Large\@xivpt{18}}
-\newcommand\LARGE{\@setfontsize\LARGE\@xviipt{22}}
-\newcommand\huge{\@setfontsize\huge\@xxpt{25}}
-\newcommand\Huge{\@setfontsize\Huge\@xxvpt{30}}
-\newcommand\footnotesize{%
-   \@setfontsize\footnotesize\@viiipt{9.5}%
-\newcommand\small{%
-   \@setfontsize\small\@ixpt{11}%
-\renewcommand\normalsize{%
-   \@setfontsize\normalsize\@xpt\@xiipt
-"""
+# Latex definitions are:
+latex_sizes = {
+  "\tiny", 5,
+  "\footnotesize", 8,
+  "\small", 9,
+  "\normalsize", 10,
+  "\large", 12,
+  "\Large", 14, 
+  "\LARGE", 17,
+  "\huge", 20, 
+  "\Huge", 25,
+  }
 
 class PowerIpe:
+  def __init__(self, args):
+    self.args = args
+    if self.args.latex:
+      self.args.text = True
+
+  def findColors(self):
+    s = self.sheets.allNames(self.sheets, "color")
+    self.colors = {}
+    for k in s:
+      sym = s[k]
+      abs = self.sheets.find(self.sheets, "color", sym)
+      h = "%02x%02x%02x" % (int(abs.r * 255), int(abs.g * 255), int(abs.b * 255))
+      rgb = pptx.dml.color.RGBColor.from_string(h)
+      self.colors[sym] = rgb
+    
   def convertTime(self, s):
     return datetime.datetime(int(s[2:6]),
                              int(s[6:8]),
@@ -50,33 +66,68 @@ class PowerIpe:
                              int(s[12:14]),
                              int(s[14:]))
 
+  def addSvg(self, slide, pageNo, svgobjs):
+    doc1 = ipe.Document()
+    doc1.replaceSheets(doc1, self.sheets.clone(self.sheets))
+    doc1.setProperties(doc1, self.iprops)
+    p1 = doc1[1]
+    p = self.doc[pageNo]
+    for i in svgobjs:
+      obj = p[i]
+      p1.insert(p1, None, obj.clone(obj), None, "alpha")
+    tmpname = "tmpipe%d.ipe" % pageNo
+    svgname = "tmpipe%d.svg" % pageNo
+    pngname = "tmpipe%d.png" % pageNo
+    doc1.save(doc1, tmpname, "xml", None)
+    os.system("iperender -svg -nocrop %s %s" % (tmpname, svgname))
+    os.system("iperender -png -nocrop -transparent %s %s" % (tmpname, pngname))
+    pic = slide.shapes.add_svg_picture(svgname, pngname, 0, 0)
+  
   def convertText(self, slide, obj):
-    mp = obj.get(obj, "minipage")
-    if not mp:
-      return False
     s = obj.text(obj)
-    matrix = ipe.Matrix()
     textStyle = obj.get(obj, "textstyle")
     textSize = obj.get(obj, "textsize")
     color = obj.get(obj, "stroke")
     box = ipe.Rect()
-    obj.addToBBox(obj, box, matrix)
+    obj.addToBBox(obj, box, ipe.Matrix())
     pos = box.topLeft(box)
-    #print(textSize, textStyle, color)
-    w = LatexWalker(s)
-    nodes, lpos, llen = w.get_latex_nodes(pos=0)
-    #print(nodes, lpos, llen)
-    txBox = slide.shapes.add_textbox(Pt(self.x0 + pos.x), Pt(self.y0 - pos.y),
+    # why do we need this correction factor?
+    size = sizeMap.get(textSize, sizeMap["normal"]) * 0.85
+    #helper = slide.shapes.add_shape(MSO_SHAPE.RECTANGLE, Pt(self.x0 + pos.x), Pt(self.y0 - pos.y),
+    # Pt(box.width(box)), Pt(box.height(box)))
+    txBox = slide.shapes.add_textbox(Pt(self.x0 + pos.x), Pt(self.y0 - pos.y - 0.2 * size),
                                      Pt(box.width(box)), Pt(box.height(box)))
     tf = txBox.text_frame
+    tf.word_wrap = True
+    tf.auto_size = MSO_AUTO_SIZE.NONE
+    tf.margin_top = 0
+    tf.margin_left = 0
+    tf.margin_right = 0
+    tf.margin_bottom = 0
+    tf.vertical_anchor = MSO_ANCHOR.TOP
     p = tf.paragraphs[0]
-    run = p.add_run()
-    run.text = s
+    p.text = s.strip()
+    p.font.size = Pt(size)
+    p.line_spacing = Pt(1.2 * size)
     if textStyle == "center":
       p.alignment = PP_ALIGN.CENTER
     elif textStyle == "item":
       p.level = 1
-    run.font.size = Pt(sizeMap.get(textSize, 12))
+    if color in self.colors:
+      p.font.color.rgb = self.colors[color]
+
+  # determine if we want to convert this text object
+  def wantConvertText(self, obj):
+    if not self.args.text:
+      return False
+    mp = obj.get(obj, "minipage")
+    if not mp:
+      return False
+    s = obj.text(obj)
+    if not self.args.latex:
+      if "\\" in s or "$" in s:
+        return False
+    return True
 
   def convertPage(self, pageNo, viewNo):
     sys.stderr.write("Converting page %d\n" % pageNo)
@@ -88,70 +139,76 @@ class PowerIpe:
       title_placeholder.text = t.title
     else:
       slide = self.prs.slides.add_slide(self.blank_slide_layout)
+    # First determine which objects we'll convert to svg
     svgobjs = set()
-    for i in range(1, len(p)):
+    for i in range(1, len(p) + 1):
       if not p.visible(p, viewNo, i):
         continue
       obj = p[i]
       ty = obj.type(obj)
-      if ty == "text":
-        if not self.convertText(slide, obj):
-          svgobjs.add(i)
+      if ty == "text" and self.wantConvertText(obj):
+        pass
       #elif ty == "image":
       #  info = obj.info(obj)
       else:
         svgobjs.add(i)
-    l = p.addLayer(p, "poweripe")
-    x = p.countViews(p) + 1
-    p.insertView(p, x, "poweripe")
-    p.setVisible(p, x, "poweripe", True)
-    for i in svgobjs:
-      p.setLayerOf(p, i, "poweripe")
-    print("Exporting!", pageNo, x)
-    self.doc.save(self.doc, "tmpipe%d.ipe" % pageNo, "xml", None)
+    self.addSvg(slide, pageNo, svgobjs)
+    # Now add remaining objects
+    for i in range(1, len(p) + 1):
+      if i in svgobjs or not p.visible(p, viewNo, i):
+        continue
+      obj = p[i]
+      ty = obj.type(obj)
+      if ty == "text":
+        self.convertText(slide, obj)
 
-  def convert(self, ipename, pptname):
-    self.doc = ipe.Document(ipename)
-    iprops = self.doc.properties(self.doc)
+  def convert(self):
+    self.doc = ipe.Document(self.args.ipefile)
+    self.sheets = self.doc.sheets(self.doc)
+    self.iprops = self.doc.properties(self.doc)
+    self.findColors()
 
     self.prs = pptx.Presentation()
-
-    props = self.prs.core_properties
-    props.author = iprops.author
-    props.last_modified_by = "" # shouldn't be python-pptx author
-    props.title = iprops.title
-    props.created = self.convertTime(iprops.created)
-    props.modified = self.convertTime(iprops.modified)
-    props.keywords = iprops.keywords
-    props.subject = iprops.subject
-    
-    style = self.doc.sheets(self.doc)
-    layout = style.find(style, "layout")
-    self.x0 = layout.origin.x
-    self.y0 = layout.origin.y + layout.papersize.y
-  
-    self.prs.slide_width = Pt(layout.papersize.x)
-    self.prs.slide_height = Pt(layout.papersize.y)
-  
-    self.bullet_slide_layout = self.prs.slide_layouts[1]
     self.title_slide_layout = self.prs.slide_layouts[5]
     self.blank_slide_layout = self.prs.slide_layouts[6]
 
-    fallback_icon = "/mnt/d/Devel/ipe/group/ipe32.png"
+    props = self.prs.core_properties
+    props.author = self.iprops.author
+    props.last_modified_by = "" # shouldn't be python-pptx author
+    props.title = self.iprops.title
+    props.created = self.convertTime(self.iprops.created)
+    props.modified = self.convertTime(self.iprops.modified)
+    props.keywords = self.iprops.keywords
+    props.subject = self.iprops.subject
+    
+    layout = self.sheets.find(self.sheets, "layout")
+    self.x0 = layout.origin.x
+    self.y0 = layout.origin.y + layout.papersize.y
+    print(layout.origin, layout.papersize, self.x0, self.y0)
+  
+    self.prs.slide_width = Pt(layout.papersize.x)
+    self.prs.slide_height = Pt(layout.papersize.y)
   
     for pageNo in range(1, len(self.doc) + 1):
       p = self.doc[pageNo]
       viewNo = p.countViews(p)
       self.convertPage(pageNo, viewNo)
-    #os.system("/mnt/d/Devel/ipe/mingw64/bin/iperender.exe -svg -page %d -nocrop %s %s" % (page + 1, ipename, svgname))
-    #pic = slide.shapes.add_svg_picture(svgname, fallback_icon, 0, 0)
-  
+
+    pptname = self.args.output
+    if pptname is None:
+      pptname = self.args.ipefile[:-4] + '.pptx'
     self.prs.save(pptname)
   
 # --------------------------------------------------------------------
 
 if __name__ == "__main__":
-  powerIpe = PowerIpe()
-  powerIpe.convert("retreat-consultancy-projects-20190910.pdf", "test.pptx")
+  parser = argparse.ArgumentParser(description="Convert an Ipe presentation into pptx format")
+  parser.add_argument('ipefile', help="File name for Ipe input")
+  parser.add_argument('--output', help="File name for pptx output")
+  parser.add_argument('--text', help="Convert text containing no Latex", action='store_true')
+  parser.add_argument('--latex', help="Try to convert Latex text", action='store_true')
+
+  powerIpe = PowerIpe(parser.parse_args())
+  powerIpe.convert()
 
 # --------------------------------------------------------------------
--- poweripe/readme.md
+++ poweripe/readme.md
@@ -9,16 +9,16 @@ presentation into a Powerpoint presentation.
 
 ## Installation
 
-Poweripe requires two Python modules, one for reading Ipe files, and
-one for creating Powerpoint documents.
+Poweripe requires some Python modules:
 
-1. Install the [ipepython](https://github.com/otfried/ipe-tools/tree/python3/ipepython) module.
+1. Install `pylatexenc` by saying `pip3 install pylatexenc`. (This module is used to parse Latex.)
 
-2. Install the `python-pptx` module.
+2. Install the [ipepython](https://github.com/otfried/ipe-tools/tree/master/ipepython) module. (This module is used to read Ipe files.)
 
+3. Install the `python-pptx` module (a module for creating Powerpoint documents). 
 Currently, you need to get my fork of this module, with added SVG
 support.  You can find it at
-https://github.com/otfried/python-pptx/tree/svg-pictures.
+https://github.com/otfried/python-pptx. Make sure to get the **svg-pictures** branch!
 
 <!--
 It requires the python-pptx module, which you can install with
@@ -34,6 +34,21 @@ You run Poweripe from the command line like this:
 ```
 python3 poweripe.py presentation.pdf
 ```
+The output will be stored in `presentation.pptx`.  Alternatively, provide the `--output` option to select a different output file.
 
-The output will be stored in `presentation.pptx`.
+In this simplest mode, Poweripe stores all the contents of the Ipe
+document as an image in the pptx output.  The image contains both an
+SVG-version, which gives high-quality vector graphics, and a
+lower-resolution bitmap as a fallback.  Recent Powerpoint versions
+will display the SVG contents, but it seems that Libreoffice does not
+yet support SVG and shows the bitmap version instead.
+
+With the `--text` option, Poweripe will convert all minipage text
+objects that do not contain Latex markup into text objects that can be
+edited in the pptx file.
+
+With the `--latex` option, Poweripe will also convert minipage text
+objects that contain Latex markup.  Translating some simple Latex
+commands (for color, emphasis, and itemizing) is planned for the near
+future.
 
