8053220c204fd580c3e3c3ed91d00df44ab17387
Otfried Cheong <otfried@ipe.airpost.net>
2019-09-12 17:25:26 +0200

    Let ipepython test script read document filename from command line

--- ipepython/test.py
+++ ipepython/test.py
@@ -15,17 +15,18 @@ g = Ipe.globals()
 print("sqrt(2) from Lua: ", g.math.sqrt(2))
 print("Global variable config: ", g.config.latexdir, g.config.platform)
 
-ipename = "../ipetopptx/flipgraphs.ipe"
+if len(sys.argv) == 2:
+  ipename = sys.argv[1]
+  
+  doc = ipe.Document(ipename)
+  print("Document has ", len(doc), "pages and ", doc.countTotalViews(doc), "views")
 
-doc = ipe.Document(ipename)
-print("Document has ", len(doc), "pages and ", doc.countTotalViews(doc), "views")
+  props = doc.properties(doc)
+  print("Some document properties: ", props.title, props.created, props.creator)
+  print("All document properties:")
+  for k in props:
+    print(" -", k, props[k])
 
-props = doc.properties(doc)
-print("Some document properties: ", props.title, props.created, props.creator)
-print("All document properties:")
-for k in props:
-  print(" -", k, props[k])
-
-for pageNo in range(1, len(doc) + 1):
-  print("Page %d has %d objects" % (pageNo, len(doc[pageNo])))
+  for pageNo in range(1, len(doc) + 1):
+    print("Page %d has %d objects" % (pageNo, len(doc[pageNo])))
 
